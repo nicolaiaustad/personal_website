c3cece45a90bec762b3f881b57866730fb1bb0a0
Nicolai Austad <nicolaau@stud.ntnu.no>
2024-02-18 18:09:29 +0530

    Changed back

--- functions/process_input.js
+++ functions/process_input.js
@@ -1,6 +1,6 @@
 // functions/process-input.js
-// const { Configuration, OpenAIApi } = require("openai");
-const OpenAIApi = require('openai').default;
+const { Configuration, OpenAIApi } = require("openai");
+// const OpenAIApi = require('openai').default;
 
 
 exports.handler = async (event) => {
